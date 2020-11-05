3b4e5e7f52fb2965e06ce6b40e6d52c70774dd88
Andrew Glavackiy <andrew.glavackiy@gmail.com>
2020-11-06 02:55:26 +0700

    Добавил реализацию таймера через Thread и обновление UI через Handler

--- app/src/main/java/com/example/timerandworker/TimerAndWorkerFragment.kt
+++ app/src/main/java/com/example/timerandworker/TimerAndWorkerFragment.kt
@@ -1,11 +1,10 @@
 package com.example.timerandworker
 
 import android.os.Bundle
-import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import android.widget.TextView
 
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.timer_and_worker_fragment.*
@@ -26,21 +25,16 @@ class TimerAndWorkerFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-
-        val handler = Handler()
-        val thread = Thread {
-            handler.post(Runnable {
-                Thread.sleep(10000)
-                Toast.makeText(context, "From thread ${Thread.currentThread()}", Toast.LENGTH_LONG)
-                    .show()
-            })
+        someButton.setOnClickListener {
+            val timer2 = TimerHandlerThread(someText)
+            timer2.start()
+            someButton.isEnabled = false
         }
 
         startTimer.setOnClickListener {
-            //Toast.makeText(context, "Starting new Timer", Toast.LENGTH_LONG).show()
-            thread.start()
-           // startTimer.isEnabled = false
+            val timer = TimerHandlerThread(time)
+            timer.start()
+            startTimer.isEnabled = false
         }
-
     }
 }
--- app/src/main/java/com/example/timerandworker/TimerHandlerThread.kt
+++ app/src/main/java/com/example/timerandworker/TimerHandlerThread.kt
@@ -1,7 +1,48 @@
 package com.example.timerandworker
 
-class TimerHandlerThread: Thread() {
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import android.widget.TextView
 
 
+class TimerHandlerThread constructor(_textView: TextView) : Thread() {
 
+    private val textView : TextView = _textView
+    var handler = Handler(Looper.getMainLooper())
+
+    override fun run() {
+        var secondsCounter = 0
+        var formatTime: String
+
+        while (true) {
+            formatTime = convertCounterIntoFormatTime(secondsCounter)
+
+            handler.post {
+                textView.text = formatTime
+                Log.i("TimerTick", "Now count = $formatTime")
+            }
+
+            sleep(1000)
+            secondsCounter++
+        }
+    }
+
+    private fun convertCounterIntoFormatTime(counter: Int): String{
+        var formatSeconds = "0"
+        var formatMinutes = "0"
+
+        if (counter % 60 < 10) {
+            formatSeconds = "0${counter % 60}"
+        } else if (counter % 60 in 10..60) {
+            formatSeconds = "${counter % 60}"
+        }
+
+        if (counter / 60 < 10) {
+            formatMinutes = "0${counter / 60}"
+        } else if (counter / 60 in 10..60) {
+            formatMinutes = "${counter / 60}"
+        }
+        return "$formatMinutes:$formatSeconds"
+    }
 }
